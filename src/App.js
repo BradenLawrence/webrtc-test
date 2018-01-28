974716c4ebe644743142353b7ba8a08e02c4c001
blip <b@bradenlawrence.com>
2018-01-28 15:26:28 -0500

    add simple implementation of getusermedia with start and stop functions

--- src/App.js
+++ src/App.js
@@ -2,17 +2,43 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+const constraints = {
+  video:  {
+    width:  800,
+    height: 600
+  },
+  audio:  false
+}
+
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      videoSource: null
+    }
+  }
+
+  initStream() {
+    navigator.mediaDevices.getUserMedia(constraints)
+    .then( stream => this.setState({ videoSource: stream }, () => { this.video.srcObject = this.state.videoSource }) )
+  }
+  
+  stopStream() {
+    this.state.videoSource.getTracks()[0].stop()
+  }
+
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Web RTC Test</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <video ref={ video => this.video = video } width={ constraints.video.width } height={ constraints.video.height } autoPlay="true" />
+        <button onClick={ () => {this.initStream()} }>Start recording</button>
+        <button onClick={ () => {this.stopStream()} }>Stop recording</button>
       </div>
     );
   }
