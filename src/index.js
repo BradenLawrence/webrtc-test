55def3ff0894ba50685ee97c08d6549a29585ed0
blip <b@bradenlawrence.com>
2018-01-31 01:51:51 -0500

    setup index to use redux store and middleware

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React                            from 'react'
+import ReactDOM                         from 'react-dom'
+import { Provider }                     from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import promise                          from 'redux-promise'
+import App                              from './App'
+import { rootReducer }                  from './reducers'
+import registerServiceWorker            from './registerServiceWorker'
+import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore)
+
+ReactDOM.render(
+    <Provider store={createStoreWithMiddleware(rootReducer)}>
+        <App />
+    </Provider>
+    , document.getElementById('root')
+)
+registerServiceWorker()
