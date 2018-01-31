9d34df99a7d63a22fa995f69c1aede217a77c9e3
blip <b@bradenlawrence.com>
2018-01-31 01:55:02 -0500

    rebuild VideoContainer to use redux  - attach state to redux store  - pull video component structure into this file as it is no longer needed  - add new start and stop functions to call the redux actions

--- src/containers/VideoContainer.js
+++ src/containers/VideoContainer.js
@@ -1,33 +1,58 @@
-import React, { Component } from 'react'
-import { Video }            from '../components/Video';
+import React, { Component }         from 'react'
+import { connect }                  from 'react-redux'
+import { bindActionCreators }       from 'redux'
+import { StartStream, StopStream }  from '../actions'
 
-class VideoContainer extends Component {
-    render() {
-        const controls = {
-            acquireStream(constraints) {
-              return navigator.mediaDevices.getUserMedia(constraints)
-              .then( stream => { return stream } )
-            },
-            stop(stream) {
-              stream.getTracks()[0].stop()
-            }
-          }
-      
-        const constraints = {
-        video: {
-            width:  800,
-            height: 600
-        },
-        audio:  false
-        }
+class Video extends Component {
+    constructor(props) {
+        super(props)
+        this.start  = this.start.bind(this)
+        this.stop   = this.stop.bind(this)
+    }
+
+    start(event) {
+        event.preventDefault()
+        this.props.StartStream(this.props.constraints)
+    }
+
+    stop(event) {
+        event.preventDefault()
+        this.props.StopStream(this.props.stream)
+    }
+
+    componentWillReceiveProps(nextProps) {
+        console.log('Incomming props: ' + nextProps)
+        this.video.srcObject = nextProps.stream
         
+    }
+
+    render() {
         return (
-            <Video 
-                controls    = { controls }
-                constraints = { constraints }
-            />
+            <div className="video-frame">
+                <video 
+                    ref         = { video => this.video = video } 
+                    width       = { this.props.constraints.video.width } 
+                    height      = { this.props.constraints.video.height } 
+                    autoPlay    = "true" 
+                />
+                <button onClick={ this.start }>Start recording</button>
+                <button onClick={ this.stop }>Stop recording</button>
+            </div>
         )
     }
 }
+
+function mapStateToProps(state) {
+    return {
+        stream:         state.stream,
+        constraints:    state.constraints
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({ StartStream, StopStream }, dispatch)
+}
+
+const VideoContainer = connect(mapStateToProps, mapDispatchToProps)(Video)
 
 export { VideoContainer }
