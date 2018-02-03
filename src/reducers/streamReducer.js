fa9cbe92bd77308f4b716e2118f5a8ed62488336
blip <b@bradenlawrence.com>
2018-02-03 13:24:28 -0500

    remove console logs

--- src/reducers/streamReducer.js
+++ src/reducers/streamReducer.js
@@ -2,8 +2,7 @@ import { START_STREAM, STOP_STREAM } from '../actions'
 
 const streamReducer = function(state = {}, action) {
     switch(action.type) {
-        case START_STREAM: 
-            console.log('Reducer says: ' + action.payload)
+        case START_STREAM:
             return action.payload
         case STOP_STREAM: 
             action.payload.getTracks()[0].stop()
