53db23f6f03b9695a6bce73658b6b0e6ad39d478
blip <b@bradenlawrence.com>
2018-02-16 21:01:26 -0500

    remove react-share package  - was not able to add images inline  - replacing it with a standard download button for now

--- src/components/ShareModal.js
+++ src/components/ShareModal.js
@@ -1,16 +1,5 @@
 import React, { Component }     from 'react'
 import Modal                    from 'react-modal'
-import { FacebookShareButton,
-         TwitterShareButton,
-         GooglePlusShareButton,
-         WhatsappShareButton,
-         EmailShareButton,
-    
-         FacebookIcon,
-         TwitterIcon,
-         GooglePlusIcon,
-         WhatsappIcon,
-         EmailIcon }            from 'react-share'
 
 class ShareModal extends Component {
 
@@ -22,25 +11,11 @@ class ShareModal extends Component {
                 overlayClassName="share-overlay"
             >
                 <img className="frame" src={ this.props.url } alt="" />
-                <div className="share-wrapper">
-                    <TwitterShareButton     className="share-button" url={ this.props.url }>
-                        <TwitterIcon    size={40} round={true} />
-                    </TwitterShareButton>
-                    <FacebookShareButton    className="share-button" url={ this.props.url }>
-                        <FacebookIcon   size={40} round={true} />
-                    </FacebookShareButton>
-                    <GooglePlusShareButton  className="share-button" url={ this.props.url }>
-                        <GooglePlusIcon size={40} round={true} />
-                    </GooglePlusShareButton>
-                    <WhatsappShareButton    className="share-button" url={ this.props.url }>
-                        <WhatsappIcon   size={40} round={true} />
-                    </WhatsappShareButton>
-                    <EmailShareButton       className="share-button" url={ this.props.url }>
-                        <EmailIcon      size={40} round={true} />
-                    </EmailShareButton>
-                </div>
                 <div className="button-restart">
-                    <button onClick= { this.props.onClose } >{ this.props.closeText }</button>
+                    <a href={ this.props.url } download="photobooth.gif">
+                    <button className="button-download">Download</button>
+                    </a><br/>
+                    <button className="button-restart" onClick= { this.props.onClose } >{ this.props.closeText }</button>
                 </div>
             </Modal>
         )
